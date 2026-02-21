7dda5bdd55b187441c5afc1b269fbfc16f791f6f
Huseyin YONTEM <hyontem071@posta.pau.edu.tr>
2026-02-21 13:32:28 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: '30efa28dfaa780bd99dcedbd9b1d63ad'
+     },
+     {
+       title: 'Contact',
+       pageId: '30efa28dfaa780df970af08310843a10'
+     }
+   ]
 })
